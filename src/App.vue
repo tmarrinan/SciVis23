d1ea1bd014c4c044f9a6c79a16dab4c6be4a6a66
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2023-01-09 15:58:40 -0600

    add area ids and colors to point cloud

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script>
-import $ from 'jquery'
+import $ from 'jquery';
 import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { Color4 } from "@babylonjs/core/Maths/math.color";
 import { PointsCloudSystem } from "@babylonjs/core/Particles/pointsCloudSystem";
 import { CreateGround } from "@babylonjs/core/Meshes/Builders/groundBuilder";
 import { Scene } from "@babylonjs/core/scene";
@@ -13,7 +14,56 @@ import { GridMaterial } from "@babylonjs/materials/grid/gridMaterial";
 export default {
     data() {
         return {
-            test: "hi there"
+            area_colors: [ // unique color generator: https://mokole.com/palette.html
+                Color4.FromHexString('#808080'),
+                Color4.FromHexString('#2f4f4f'),
+                Color4.FromHexString('#556b2f'),
+                Color4.FromHexString('#6b8e23'),
+                Color4.FromHexString('#a0522d'),
+                Color4.FromHexString('#8b0000'),
+                Color4.FromHexString('#483d8b'),
+                Color4.FromHexString('#008000'),
+                Color4.FromHexString('#3cb371'),
+                Color4.FromHexString('#b8860b'),
+                Color4.FromHexString('#bdb76b'),
+                Color4.FromHexString('#008b8b'),
+                Color4.FromHexString('#4682b4'),
+                Color4.FromHexString('#000080'),
+                Color4.FromHexString('#d2691e'),
+                Color4.FromHexString('#9acd32'),
+                Color4.FromHexString('#32cd32'),
+                Color4.FromHexString('#8fbc8f'),
+                Color4.FromHexString('#8b008b'),
+                Color4.FromHexString('#b03060'),
+                Color4.FromHexString('#ff4500'),
+                Color4.FromHexString('#ffa500'),
+                Color4.FromHexString('#ffd700'),
+                Color4.FromHexString('#6a5acd'),
+                Color4.FromHexString('#ffff00'),
+                Color4.FromHexString('#0000cd'),
+                Color4.FromHexString('#00ff00'),
+                Color4.FromHexString('#ba55d3'),
+                Color4.FromHexString('#8a2be2'),
+                Color4.FromHexString('#00ff7f'),
+                Color4.FromHexString('#dc143c'),
+                Color4.FromHexString('#00ffff'),
+                Color4.FromHexString('#00bfff'),
+                Color4.FromHexString('#f4a460'),
+                Color4.FromHexString('#f08080'),
+                Color4.FromHexString('#adff2f'),
+                Color4.FromHexString('#ff6347'),
+                Color4.FromHexString('#b0c4de'),
+                Color4.FromHexString('#ff00ff'),
+                Color4.FromHexString('#6495ed'),
+                Color4.FromHexString('#dda0dd'),
+                Color4.FromHexString('#ff1493'),
+                Color4.FromHexString('#afeeee'),
+                Color4.FromHexString('#98fb98'),
+                Color4.FromHexString('#7fffd4'),
+                Color4.FromHexString('#ffe4b5'),
+                Color4.FromHexString('#ff69b4'),
+                Color4.FromHexString('#ffb6c1')
+            ]
         }
     },
     methods: {
@@ -38,9 +88,9 @@ export default {
                     dataType: 'text',
                     url: url,
                     success: (response) => {
-                        //console.log(response);
                         let csv = response.split(/\r?\n/).filter(el => el.length !== 0)
-                                          .map(line => line.split(',').map(x => parseFloat(x)));
+                                          .map(line => line.split(','));
+                        
                         resolve(csv);
                     },
                     error: (status, message) => {
@@ -84,11 +134,20 @@ export default {
         // Download brain position data and create point cloud
         this.getCSV('/data/viz-no-network_positions.csv')
         .then((neuron_positions) => {
+            $.each(neuron_positions, (index) => {
+                neuron_positions[index][0] = parseFloat(neuron_positions[index][0]);
+                neuron_positions[index][1] = parseFloat(neuron_positions[index][1]);
+                neuron_positions[index][2] = parseFloat(neuron_positions[index][2]);
+                neuron_positions[index][3] = parseInt(neuron_positions[index][3]);
+            });
+        
             let pcs = new PointsCloudSystem("pcs", 3, scene);
             pcs.addPoints(neuron_positions.length, (particle, i, s) => {
+            let area_idx = neuron_positions[s][3];
                 particle.position = new Vector3(neuron_positions[s][0],
                                                 neuron_positions[s][1], 
                                                 neuron_positions[s][2]);
+                particle.color = this.area_colors[area_idx];
             });
             pcs.buildMeshAsync()
             .then((mesh) => {
@@ -114,9 +173,6 @@ export default {
         ground.material = material;
         
         
-
-
-
 
         // Render every frame
         engine.runRenderLoop(() => {
