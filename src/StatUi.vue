4ee13d9706d5b731c533362b693dd35843af2a23
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2023-06-27 11:17:54 -0500

    statui add room interface

--- src/StatUi.vue
+++ src/StatUi.vue
@@ -5,7 +5,45 @@ export default {
     data() {
         return {
             ws: null,
-            ws_open: false
+            ws_open: false,
+            joined_room: false,
+            room_id: ''
+        }
+    },
+    methods: {
+        joinRoom(event) {
+            if (this.room_id !== '' && this.ws_open) {
+                let message = {type: 'join', data: {id: this.room_id}};
+                this.ws.send(JSON.stringify(message));
+            }
+        },
+
+        createRoom(event) {
+            if (this.room_id !== '' && this.ws_open) {
+                let message = {type: 'create', data: {id: this.room_id}};
+                this.ws.send(JSON.stringify(message));
+            }
+        },
+
+        leaveRoom(event) {
+            let message = {type: 'leave', data: {id: this.room_id}};
+            this.ws.send(JSON.stringify(message));
+            this.joined_room = false;
+            this.room_id = '';
+        },
+
+        joinedRoom(type, success) {
+            if (success) {
+                this.joined_room = true;
+            }
+            else {
+                if (type === 'create') {
+                    alert('Error: Could not create room. Try a different ID.');
+                }
+                else if (type === 'join') {
+                    alert('Error: Could not join room. Try creating one or join using a different ID.');
+                }
+            }
         }
     },
     mounted() {
@@ -24,6 +62,12 @@ export default {
         };
         this.ws.onmessage = (event) => {
             let message = JSON.parse(event.data);
+            if (message.type === 'create' || message.type === 'join') {
+                this.joinedRoom(message.type, message.response === 'success');
+            }
+            else {
+                console.log(message);
+            }
         };
 
         // D3 chart
@@ -32,7 +76,7 @@ export default {
             {name: 'Thing 2', value: 2},
             {name: 'Thing 3', value: 9},
             {name: 'Thing 4', value: 7}
-        ]
+        ];
         let margin = {top: 20, right: 20, bottom: 30, left: 40};
         let width = 600 - margin.left - margin.right;
         let height = 400 - margin.top - margin.bottom;
@@ -65,10 +109,30 @@ export default {
 </script>
 
 <template>
-    <svg id="d3-chart"></svg>
+    <div class="box">
+        <div class="row header">
+            <label>Room:</label>
+            <div v-if="!joined_room" style="display: inline;">
+                <input class="text-input" type="text" placeholder="Enter ID" v-model="room_id"/>
+                <button :class="'button-input ' + (ws_open ? 'join-btn' : 'disable-btn')" type="button" @click="joinRoom">Join</button>
+                <button :class="'button-input ' + (ws_open ? 'create-btn' : 'disable-btn')" type="button" @click="createRoom">Create</button>
+            </div>
+            <div v-else style="display: inline;">
+                <label class="text-display">{{ room_id }}</label>
+                <button :class="'button-input ' + (ws_open ? 'leave-btn' : 'disable-btn')" type="button" @click="leaveRoom">Leave</button>
+            </div>
+        </div>
+        <div class="row content">
+            <svg id="d3-chart"></svg>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+input, select, option, button {
+    font-size: 1rem;
+}
+
 svg :deep(.d3bar) {
     stroke: #4362A4;
     stroke-width: 5px;
@@ -83,4 +147,64 @@ svg :deep(.d3axis) {
     font-size: 1rem;
 }
 
+.box {
+    display: flex;
+    flex-flow: column;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    background-color: #3C3C3C;
+    color: #FFFFFF;
+}
+
+.box .row.header {
+    flex: 0 1 auto;
+    width: 100%;
+    padding: 0.5rem;
+    border-bottom: solid 1px #CCCCCC;
+}
+
+.box .row.content {
+    flex: 1 1 auto;
+}
+
+.text-input {
+    width: 8rem;
+    margin: 0 0.5rem;
+}
+
+.button-input {
+    width: 5rem;
+    height: 1.5rem;
+    border-radius: 0.25rem;
+    border: none;
+    color: #FFFFFF;
+    margin: 0 0.5rem 0 0;
+}
+
+.join-btn {
+    background-color: #4A9056;
+}
+
+.create-btn {
+    background-color: #585EAC;
+}
+
+.leave-btn {
+    background-color: #931F1F;
+}
+
+.disable-btn {
+    background-color: #CDCDCD;
+    color: #636363;
+}
+
+.text-display {
+    display: inline-block;
+    background-color: #DCDCDC;
+    color: #000000;
+    width: 7.6rem;
+    margin: 0 0.5rem;
+    padding: 0.2rem;
+}
 </style>
