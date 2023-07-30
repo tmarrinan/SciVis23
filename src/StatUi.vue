38240877ad02c985ef6e8d108d7933e038284d31
Madeleine Moeller <moel6746@stthomas.edu>
2023-07-30 14:20:03 -0500

    chart js

--- src/StatUi.vue
+++ src/StatUi.vue
@@ -1,6 +1,7 @@
 <script>
 import * as d3 from 'd3';
 import timeline from './components/timeline'
+import Chart from 'chart.js/auto';
 
 
 
@@ -10,26 +11,26 @@ export default {
             ws: null,
             ws_open: false,
             joined_room: false,
-            room_id: '', 
+            room_id: '',
         }
     },
     methods: {
         joinRoom(event) {
             if (this.room_id !== '' && this.ws_open) {
-                let message = {type: 'join', data: {id: this.room_id}};
+                let message = { type: 'join', data: { id: this.room_id } };
                 this.ws.send(JSON.stringify(message));
             }
         },
 
         createRoom(event) {
             if (this.room_id !== '' && this.ws_open) {
-                let message = {type: 'create', data: {id: this.room_id}};
+                let message = { type: 'create', data: { id: this.room_id } };
                 this.ws.send(JSON.stringify(message));
             }
         },
 
         leaveRoom(event) {
-            let message = {type: 'leave', data: {id: this.room_id}};
+            let message = { type: 'leave', data: { id: this.room_id } };
             this.ws.send(JSON.stringify(message));
             this.joined_room = false;
             this.room_id = '';
@@ -38,7 +39,7 @@ export default {
         joinedRoom(type, success) {
             if (success) {
                 this.joined_room = true;
-                this.ws.send(JSON.stringify({type: 'getState'}));
+                this.ws.send(JSON.stringify({ type: 'getState' }));
             }
             else {
                 if (type === 'create') {
@@ -57,10 +58,40 @@ export default {
     mounted() {
         // WebSocket
         //this.ws = new WebSocket('wss://gliese.cs.stthomas.edu:8008');
+
+        let myChart = null;
+
         this.ws = new WebSocket('wss://scivis23-ws.onrender.com');
         this.ws.onopen = (event) => {
             console.log('WebSocket connected!');
             this.ws_open = true;
+
+            myChart = new Chart(document.getElementById('myChart'), {
+                type: 'bar',
+                data: {
+                    labels: null,
+                    datasets: [{
+                        label: null,
+                        data: null,
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            title: {
+                                display: true,
+                                text: '# of Neurons'
+                            }
+                        }
+                    }
+
+                }
+            });
+
+
+
+
         };
         this.ws.onclose = (event) => {
             console.log('WebSocket closed');
@@ -81,225 +112,163 @@ export default {
                 console.log(message.data);
             }
 
-        let view = message.data.view +1
-        console.log("view: " + view)
-        console.log("simulation: " + message.data.state.simulation)
-        console.log("neuron property: " + message.data.state.neuron_property)
-        console.log("timestep: " + message.data.state.timestep)
+            let view = message.data.view + 1
+            console.log("view: " + view)
+            console.log("simulation: " + message.data.state.simulation)
+            console.log("neuron property: " + message.data.state.neuron_property)
+            console.log("timestep: " + message.data.state.timestep)
 
 
-        this.timeline = new timeline.Timeline();
-        let global_mm = {};
+            this.timeline = new timeline.Timeline();
 
-   
-    
-    //for(let i = 0; i<1000;i++){//for mins and maxes - delete after
+            this.timeline.setTimestep(message.data.state.timestep);
+            this.timeline.setSimulation(message.data.state.simulation);
+            this.timeline.getData()
+                .then((table) => {
 
-        
-
-        this.timeline.setTimestep(message.data.state.timestep);
-        //this.timeline.setTimestep(i);
-
-        this.timeline.setSimulation(message.data.state.simulation);
-        //this.timeline.getTimestep()
-        this.timeline.getData()
-        .then((table) => {
-
-            console.log(table)
+                    console.log(table)
 
 
-            let sim_data = {};
-            let desired_columns = ['current_calcium', 'target_calcium', 'fired', 'fired_fraction', 'grown_axons',
-                                   'grown_dendrites', 'connected_axons', 'connected_dendrites'];
-            for (let i = 0; i < table.neurons.schema.fields.length; i++) {
-                if (desired_columns.includes(table.neurons.schema.fields[i].name)) {
-                    sim_data[table.neurons.schema.fields[i].name] = table.neurons.data[0].children[i].values;
-                }
-            }
-
-            let sim_data_ranges = {};
-            for (let key in sim_data) {
-                if (sim_data.hasOwnProperty(key)) {
-                    if (key === 'target_calcium') {
-                        sim_data[key] = sim_data[key].map((value, index) => sim_data['current_calcium'][index] - value);
-                    }
-                    let d_min = sim_data[key][0];
-                    let d_max = sim_data[key][0];
-                    for (let i = 1; i < sim_data[key].length; i++) {
-                        let val = sim_data[key][i];
-                        d_min = val < d_min ? val : d_min;
-                        d_max = val > d_max ? val : d_max;
-                    }
-                    sim_data_ranges[key] = {min: d_min, max: d_max};
-
-                    //----globals-----
-                    // let g_min = null;
-                    // let g_max = null;
-                    // if(global_mm[key] == null || sim_data_ranges[key].min < global_mm[key].min){
-                    //     g_min = sim_data_ranges[key].min
-                    // } else {
-                    //     g_min = global_mm[key].min;
-                    // }
-                    
-                    // if(global_mm[key] == null || sim_data_ranges[key].max > global_mm[key].max){
-                    //     g_max = sim_data_ranges[key].max
-                    // } else {
-                    //     g_max = global_mm[key].max;
-                    // }
-                    // global_mm[key] = {min: g_min, max: g_max};
-                }
-            };
-
-            // console.log(i)
-          
-            // console.log(sim_data_ranges)
-            // console.log(global_mm)
-            
-            let data = [];
-
-            let fired = 0;
-            let notfired = 0;
-
-            let val_1 = 0;
-            let val_2 = 0;
-            let val_3 = 0;
-            let val_4 = 0;
-
-            let property = message.data.state.neuron_property;
-            if(message.data.state.neuron_property == 'fired'){
-                
-                for(let v = 0; v < table.neurons.numRows; v++){
-                    if(sim_data.fired[v] == 1){
-                        fired += 1
-                    } else { 
-                        notfired +=1
-                    }
-                    
+                    let sim_data = {};
+                    let desired_columns = ['current_calcium', 'target_calcium', 'fired', 'fired_fraction', 'grown_axons',
+                        'grown_dendrites', 'connected_axons', 'connected_dendrites'];
+                    for (let i = 0; i < table.neurons.schema.fields.length; i++) {
+                        if (desired_columns.includes(table.neurons.schema.fields[i].name)) {
+                            sim_data[table.neurons.schema.fields[i].name] = table.neurons.data[0].children[i].values;
+                        }
                     }
 
-                console.log("fired = " + fired)
-                console.log("not fired = " + notfired)
-                // D3 chart - for charts with binary values
-                data = [
-                    {name: 'True', value: fired},
-                    {name: 'False', value: notfired},
-                ];
-            } else {
+                    let sim_data_ranges = {};
+                    for (let key in sim_data) {
+                        if (sim_data.hasOwnProperty(key)) {
+                            if (key === 'target_calcium') {
+                                sim_data[key] = sim_data[key].map((value, index) => sim_data['current_calcium'][index] - value);
+                            }
+                            let d_min = sim_data[key][0];
+                            let d_max = sim_data[key][0];
+                            for (let i = 1; i < sim_data[key].length; i++) {
+                                let val = sim_data[key][i];
+                                d_min = val < d_min ? val : d_min;
+                                d_max = val > d_max ? val : d_max;
+                            }
+                            sim_data_ranges[key] = { min: d_min, max: d_max };
 
-                // split into 4 'categories'?
-                let min = sim_data_ranges[property].min;
-                let max = sim_data_ranges[property].max;
-                
-                let split = (max - min) / 4.0;
+
+                        }
+                    };
 
 
-                for(let v = 0; v < table.neurons.numRows; v++){
-                    if(sim_data[property][v] <= (min + split)  ){
-                        val_1 += 1;
-                    } else if (sim_data[property][v] >= (min+split) && sim_data[property][v] <= (min+(split*2))) {
-                        val_2 += 1;
-                    } else if (sim_data[property][v] >= (min+(split*2)) && sim_data[property][v] <= (min+(split*3))) {
-                        val_3 += 1;
+                    let data = [];
+
+                    let fired = 0;
+                    let notfired = 0;
+
+                    let val_1 = 0;
+                    let val_2 = 0;
+                    let val_3 = 0;
+                    let val_4 = 0;
+
+                    let property = message.data.state.neuron_property;
+                    if (message.data.state.neuron_property == 'fired') {
+
+                        for (let v = 0; v < table.neurons.numRows; v++) {
+                            if (sim_data.fired[v] == 1) {
+                                fired += 1
+                            } else {
+                                notfired += 1
+                            }
+
+                        }
+
+                        console.log("fired = " + fired)
+                        console.log("not fired = " + notfired)
+                        // D3 chart - for charts with binary values
+                        data = [
+                            { name: 'True', value: fired },
+                            { name: 'False', value: notfired },
+                        ];
                     } else {
-                        val_4 += 1;
+
+                        // split into 4 'categories'?
+                        let min = sim_data_ranges[property].min;
+                        let max = sim_data_ranges[property].max;
+
+                        let split = (max - min) / 4.0;
+
+
+                        for (let v = 0; v < table.neurons.numRows; v++) {
+                            if (sim_data[property][v] <= (min + split)) {
+                                val_1 += 1;
+                            } else if (sim_data[property][v] >= (min + split) && sim_data[property][v] <= (min + (split * 2))) {
+                                val_2 += 1;
+                            } else if (sim_data[property][v] >= (min + (split * 2)) && sim_data[property][v] <= (min + (split * 3))) {
+                                val_3 += 1;
+                            } else {
+                                val_4 += 1;
+                            }
+                        }
+                        console.log("val 1 = " + parseFloat(val_1))
+                        console.log("val 2 = " + parseFloat(val_2))
+                        console.log("val 3 = " + parseFloat(val_3))
+                        console.log("val 4 = " + parseFloat(val_4))
+
+                        // D3 chart
+                        data = [
+                            { name: min.toFixed(3) + ' - ' + (min + split).toFixed(3), value: val_1 },
+                            { name: (min + split).toFixed(3) + ' - ' + (min + (split * 2)).toFixed(3), value: val_2 },
+                            { name: (min + (split * 2)).toFixed(3) + ' - ' + (min + (split * 3)).toFixed(3), value: val_3 },
+                            { name: (min + (split * 3)).toFixed(3) + ' - ' + (max).toFixed(3), value: val_4 }
+                        ];
                     }
-                }
-                console.log("val 1 = " + parseFloat(val_1))
-                console.log("val 2 = " + parseFloat(val_2))
-                console.log("val 3 = " + parseFloat(val_3))
-                console.log("val 4 = " + parseFloat(val_4))
-
-                // D3 chart
-                data = [
-                    {name: min.toFixed(3) + ' - ' + (min + split).toFixed(3), value: val_1},
-                    {name: (min + split).toFixed(3) + ' - ' + (min + (split*2)).toFixed(3), value: val_2},
-                    {name: (min + (split*2)).toFixed(3) + ' - ' + (min + (split*3)).toFixed(3), value: val_3},
-                    {name: (min + (split*3)).toFixed(3) + ' - ' + (max).toFixed(3), value: val_4}
-                ];
-            }
-           
-
-           
-
-           
-    
-
-        
-            
-        d3.selectAll("svg > *").remove();
-
-    
-        
-        let margin = {top: 20, right: 20, bottom: 150, left: 150};
-        let width = 600 - margin.left - margin.right;
-        let height = 400 - margin.top - margin.bottom;
-        let d3svg = d3.select('#d3-chart').attr('width', width + margin.left + margin.right)
-                                          .attr('height', height + margin.bottom + margin.top)
-                      .append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-       
-                     
-                    
-        let x_axis = d3.scaleBand().range([0, width]).padding(0.1);
-        let y_axis = d3.scaleLinear().range([height, 0]);
-
-        x_axis.domain(data.map(elem => elem.name));
-        y_axis.domain([0, d3.max(data, d => d.value)]);
-        
-        d3svg.selectAll('.d3bar').data(data).enter().append('rect')
-             .classed('d3bar', true)
-             .attr('x', d => x_axis(d.name))
-             .attr('width', x_axis.bandwidth())
-             .attr('y', d =>  y_axis(d.value))
-             .attr('height', d => (height - y_axis(d.value)))
-             .style('stroke-dasharray', d => ((x_axis.bandwidth() + height - y_axis(d.value)) + ',' + x_axis.bandwidth()));
-               
-        
-
-        d3svg.append('g').classed('d3axis', true)
-                         .attr('transform', 'translate(0,' + height + ')')
-                         .call(d3.axisBottom(x_axis));
 
 
-    d3svg.append("text")
-        .attr("x", width/2)
-        .attr("y", -height)
-        .attr("text-anchor", "middle")
-        .style("font-size", "16px")
-        .style('fill', 'DarkCyan')
-        .text("Displaying View " + view);
 
-        // text label for the x axis
-        d3svg.append("text")      
-            .attr("transform", "translate(" + (width/2) + " ," + (height+40) + ")")
-            .attr("text-anchor", "middle")
-            .style("font-size", "16px")
-            .attr("text-anchor", "middle")
-            .style("font-size", "30px")
-            .style('fill', 'DarkCyan')
-            .text(property);
+                    function addData(chart, label, newData) {
+                        chart.data.labels.push(label);
+                        chart.data.datasets.forEach((dataset) => {
+                            dataset.data.push(newData);
+                        });
+                        chart.update('none');
+                    }
 
-        d3svg.append('g').classed('d3axis', true)
-                         .call(d3.axisLeft(y_axis));
-  
-        d3svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("x", -(height/2))
-            .attr("y", -60)
-            .style("font-size", "30px")
-            .style("text-anchor", "middle")   
-            .style('fill', 'DarkCyan')        
-            .text("Number of Neurons");
-        })
-        .catch((reason) => { console.error(reason); });
+                    console.log(myChart.data.datasets[0].label = property)
+
+                    function removeData(chart) {
+                        chart.data.labels.pop();
+                        chart.data.datasets.forEach((dataset) => {
+                            dataset.data.pop();
+                        });
+                        chart.update('none');
+                    }
+
+
+
+
+                    var label = data.map(function (e) {
+                        return e.name;
+                    })
+                    var values = data.map(function (e) {
+                        return e.value;
+                    })
+
+
+
+
+                    for (let i = 0; i < data.length; i++) {
+                        removeData(myChart)
+                    }
+
+
+                    for (let i = 0; i < data.length; i++) {
+                        addData(myChart, label[i], values[i]);
+                    }
+
+
+                })
+                .catch((reason) => { console.error(reason); });
         };
 
-        
-    //}//delete this for the for loop
-    
-        
-
-        }
+    }
 }
 </script>
 
@@ -308,23 +277,38 @@ export default {
         <div class="row header">
             <label>Room:</label>
             <div v-if="!joined_room" style="display: inline;">
-                <input class="text-input" type="text" placeholder="Enter ID" v-model="room_id"/>
-                <button :class="'button-input ' + (ws_open ? 'join-btn' : 'disable-btn')" type="button" @click="joinRoom">Join</button>
-                <button :class="'button-input ' + (ws_open ? 'create-btn' : 'disable-btn')" type="button" @click="createRoom">Create</button>
+                <input class="text-input" type="text" placeholder="Enter ID" v-model="room_id" />
+                <button :class="'button-input ' + (ws_open ? 'join-btn' : 'disable-btn')" type="button"
+                    @click="joinRoom">Join</button>
+                <button :class="'button-input ' + (ws_open ? 'create-btn' : 'disable-btn')" type="button"
+                    @click="createRoom">Create</button>
             </div>
             <div v-else style="display: inline;">
                 <label class="text-display">{{ room_id }}</label>
-                <button :class="'button-input ' + (ws_open ? 'leave-btn' : 'disable-btn')" type="button" @click="leaveRoom">Leave</button>
+                <button :class="'button-input ' + (ws_open ? 'leave-btn' : 'disable-btn')" type="button"
+                    @click="leaveRoom">Leave</button>
             </div>
         </div>
-        <div class="row content">
-            <svg id="d3-chart"></svg>
+
+
+        <div class="grid-container">
+            <div class="grid-x grid-padding-x">
+                <div class="cell large-12">
+                    <canvas id="myChart" height="500" width="500"></canvas>
+                </div>
+            </div>
         </div>
+
+
+
     </div>
 </template>
 
 <style scoped>
-input, select, option, button {
+input,
+select,
+option,
+button {
     font-size: 1rem;
 }
 
